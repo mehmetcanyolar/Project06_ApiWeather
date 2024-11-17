4ca2c2f4d12c8b2e793d817366fa7d766de11c86
mehmet can yolar <mehmetcanyolar@gmail.com>
2024-11-17 23:43:42 +0300

    Add project files.

---
+++ Entities/City.cs
@@ -0,0 +1,14 @@
+﻿namespace Project06_ApiWeather.Entities
+{
+    public class City
+    {
+        public int CityId { get; set; }
+        public string CityName { get; set; }
+        public string Country { get; set; }
+        public decimal Temperature { get; set; }
+        public string Detail {  get; set; }
+
+
+
+    }
+}
